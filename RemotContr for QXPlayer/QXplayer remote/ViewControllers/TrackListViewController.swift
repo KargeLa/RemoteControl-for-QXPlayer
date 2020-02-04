b6eee368c609829f7c0c63a3c0f1d1f2efb2ab21
Ilya Lagutovsky <ilyalatugovskii@gmail.com>
2020-02-04 16:56:23 +0300

    change Method

--- RemotContr for QXPlayer/QXplayer remote/ViewControllers/TrackListViewController.swift
+++ RemotContr for QXPlayer/QXplayer remote/ViewControllers/TrackListViewController.swift
@@ -35,8 +35,8 @@ class TrackListViewController: UIViewController {
         navigationController?.navigationBar.isHidden = true
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         guard let viewControllers = tabBarController?.viewControllers else { return }
         
